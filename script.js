9071f5d5c2b0ee12202f30c35227fe28082d671b
Yago <yagowns@gmail.com>
2018-07-30 17:30:45 +0100

    Update 2
    Added title, description, counter and percentages

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ var buyVolumeZero = 0;
 var totalVolume = 0;
 var sellVolumeUSDZero = 0;
 var buyVolumeUSDZero = 0;
+var percentSell = 0;
 //Starting from here, this code creates the bitmex websocket
 //and comunicates and sends trading information.
 var ws = new WebSocket('wss://www.bitmex.com/realtime?subscribe=trade:XBTUSD');
 
  ws.onopen = function () {
-     console.log('Connection Established');
+	console.log('Connection Established');
+
+	var timer = new Timer();
+	timer.start();
+	timer.addEventListener('secondsUpdated', function (e) {
+    $('.days').html(timer.getTimeValues().days);
+    $('.hours').html(timer.getTimeValues().hours);
+    $('.minutes').html(timer.getTimeValues().minutes);
+    $('.seconds').html(timer.getTimeValues().seconds);
+	});
  };
 
  ws.onmessage = function (event) { //bellow ws.onmessage - everything you want the script to do when a msg is received
@@ -23,8 +33,9 @@ var ws = new WebSocket('wss://www.bitmex.com/realtime?subscribe=trade:XBTUSD');
     var sellVolumeUSD = 0;
     var buyVolumeUSD = 0;
 
-
     totalVolume += btc;
+
+    $(".currentprice").html("1 BTC = " + xprice.toFixed(2) + " USD");
 
     if (orderside == "Sell") {
     	sellVolume = sellVolumeZero += btc; //this line adds up to the BTC sell volume
@@ -33,7 +44,6 @@ var ws = new WebSocket('wss://www.bitmex.com/realtime?subscribe=trade:XBTUSD');
         $(".red").css("width", percentSell + "vw"); //this line selects the div and apply the width atribute with percenSell var as a value
         $(".sellvolume").html(sellVolume.toFixed(8) + " BTC"); //replaces the BTC total sell volume
         $(".sellvolumeusd").html(sellVolumeUSD.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " USD"); //replaces the BTC total sell volume
-    	//console.log("Total Sold " + sellVolume);
         console.log(percentSell);
     } else if (orderside == "Buy") {
     	buyVolume = buyVolumeZero += btc; //this line adds up to the BTC buy volume
@@ -41,22 +51,23 @@ var ws = new WebSocket('wss://www.bitmex.com/realtime?subscribe=trade:XBTUSD');
         //buy percent sell does not exist because it's defined by the background
         $(".buyvolume").html(buyVolume.toFixed(8) + " BTC"); //replaces the BTC total buy volume
         $(".buyvolumeusd").html(buyVolumeUSD.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " USD"); //replaces the USD total buy volume
-    	//console.log("Total Bought " + buyVolume);
     }
 
+    var percentSellValue = percentSell.toFixed(2) + "%";
+    var percentBuyValue = (100 - percentSell).toFixed(2) + "%";
 
-    
-    
-
-
+    $(".sellpercent").html(percentSellValue); //the percent number
+    $(".buypercent").html(percentBuyValue);
 
     //console.log(orderside + " @ " + xprice + " = " + btc + " BTC " + " = " + xpent + " USD");
  };
 
  ws.onclose = function () {
      // websocket is closed.
-     console.log("Too many connections, wait a few minutes and reload page.");
+     console.log("Too many attempted connections, wait a few minutes and reload page.");
  };//This marks the end of the bitmex websocket code
+
+
 
 
 
